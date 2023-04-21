3277ee6118956843220dc7634b3cc5e47148c07b
Francesco Invitto <francescoinvitto@AirdiFrancesco.homenet.telecomitalia.it>
2023-04-21 20:57:08 +0200

    Update

--- exercise1.hs
+++
@@ -1,2 +0,0 @@
-avg list = (sum list) / (fromIntegral (length list))
-avg5 list = avg (take 5 list)
--- pythagoras.hs
+++
@@ -1,7 +0,0 @@
-triangle24perim = [(x, y, z) | x <- [1..10], y <- [1..10], z <- [1..10], x + y + z == 24, z ^ 2 == x ^ 2 + y ^ 2]
-
---optimization
---triangle24perim = [(x, y, z) | x <- [1..10], y <- [x..10], z <- [y..10], x + y + z == 24, z ^ 2 == x ^ 2 + y ^ 2]
-
---further optimization
---triangle24perim = [(x, y, z) | x <- [1..10], y <- [x..10], z <- [24 - x - y], z ^ 2 == x ^ 2 + y ^ 2]
